b74906f0781f04c76b5b7d222d39114a8ffa779c
GabrielFernandesLemos <gabriel_lemos03@hotmail.com>
2020-02-27 22:35:14 -0300

    apply event click on buy cart

--- src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -20,4 +20,16 @@ export class ShoppingCartComponent implements OnInit {
     return this.shoppingCartService.total()
   }
 
+  clear(){
+    this.shoppingCartService.clear()
+  }
+
+  removeItem(item : any){
+    this.shoppingCartService.removeItem(item)
+  }
+
+  addItem(item: any){
+    this.shoppingCartService.addItem(item)
+  }
+
 }
